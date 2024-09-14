0d11b2ad3f557af9b288744195f5f5d016bb7e9f
lukakurts <lkurtskhalia18@gmail.com>
2024-09-14 23:28:12 +0400

    changed import

--- Homework1/task_3.py
+++ Homework1/task_3.py
@@ -1,10 +1,10 @@
-import math
+from math import sqrt
 
 a = int(input("გთხოვთ შეიყვანოთ პირველი გვერდი"))
 b = int(input("გთხოვთ შეიყვანოთ მეორე გვერდი"))
 c = int(input("გთხოვთ შეიყვანოთ მესამე გვერდი"))
 p = a + b + c
 p1 = p / 2
-s = math.sqrt(p1 * (p1 - a) * (p1 - b) * (p1 - c))
+s = sqrt(p1 * (p1 - a) * (p1 - b) * (p1 - c))
 print("პერიმეტრი არის ", p)
 print("ფართობი არის ", s)
