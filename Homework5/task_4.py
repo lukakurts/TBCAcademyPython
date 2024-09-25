baf884db359043dc28619846c252d6798eb8729f
lukakurts <lkurtskhalia18@gmail.com>
2024-09-25 20:32:37 +0400

    Homework5

---
+++ Homework5/task_4.py
@@ -0,0 +1,10 @@
+number = int(input("Please enter number: "))
+
+if number < 0 or number > 50:
+    print("Please try again.")
+    exit(1)
+else:
+    print(" " * (number), " *")
+    for i in range(1, number):
+        print(" " * (number - i), "/" * i, "|", chr(92) * i)
+    print(" " * (number), " |")
